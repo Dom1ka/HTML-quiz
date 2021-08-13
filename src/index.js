e5f77c2d4f72eb6fff083d72f9ccba7c29107e94
Dom1ka <dom.soch95@gmail.com>
2021-08-13 21:55:38 +0100

    Laied out functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 const quizContainer = document.getElementById(`quiz`);
 const resultsContainer = document.getElementById(`results`);
 const submitButton = document.getElementById(`submit`);
+
+function buildQuiz() {}
+
+function showResults() {}
+
+buildQuiz();
+
+submitButton.addEventListener(`click`, showResults);
