162ea155f3758c9fc67f5f9379bbe71578409b6e
Dom1ka <dom.soch95@gmail.com>
2021-08-14 10:07:37 +0100

    Adding questions

--- src/index.js
+++ src/index.js
@@ -8,4 +8,44 @@ function showResults() {}
 
 buildQuiz();
 
-submitButton.addEventListener(`click`, showResults);
+submitButton.addEventListener(`click`, showResults); //on submit, run showResults function
+
+const myQuestions = [
+  {
+    question: "What does HTML stands for?",
+    answers: {
+      a: "Hyper Text Markup Language",
+      b: "Home Tool Markup Language",
+      c: "Hyperlinks and Text Markup Language",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "What is the difference between an opening tag and a closing tag?",
+    answers: {
+      a: "There is no difference",
+      b: "Closing tag has a / in front",
+      c: "Opening tag has a / in front",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+    },
+    correctAnswer: "",
+  },
+  {
+    question: "",
+    answers: {
+      a: "",
+      b: "",
+      c: "",
+    },
+    correctAnswer: "",
+  },
+];
